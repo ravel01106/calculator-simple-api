4a3a10c6f213abc1b8ee3b78ae37c529c4a5c58d
ravel01106 <amandaravelocabrera@gmail.com>
2024-06-03 10:40:12 +0100

    First commit

---
+++ src/test/java/com/kata/arithmetic/ArithmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.arithmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
