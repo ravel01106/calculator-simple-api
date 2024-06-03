fc0644ed21482ccbc47a811ed1df91ddfe3d5ef0
ravel01106 <amandaravelocabrera@gmail.com>
2024-06-03 11:48:16 +0100

    feat: created ArithmeticResponse and ArithmeticRequest class models

---
+++ src/main/java/com/kata/arithmetic/models/ArithmeticResponse.java
@@ -0,0 +1,17 @@
+package com.kata.arithmetic.models;
+
+public class ArithmeticResponse {
+    private String response;
+
+    public ArithmeticResponse() {
+        this.response = "0";
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
---
+++ src/main/java/com/kata/arithmetic/models/AritmeticRequest.java
@@ -0,0 +1,12 @@
+package com.kata.arithmetic.models;
+
+public class AritmeticRequest {
+    String expression;
+
+    public AritmeticRequest() {
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+}
